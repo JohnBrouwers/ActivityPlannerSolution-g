7f11b3721e139dc5743becdf4984868dc26532c3
John Brouwers <j.brouwers@isential.nl>
2021-09-22 14:50:58 +0200

    Add project files.

---
+++ ActivityPlanner/Models/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ActivityPlanner.Models
+{
+    internal class Person
+    {
+        //field: data container
+        private string surname;
+
+        //Auto implemented property:
+        public string Firstname { get; set; }
+
+        //Expression bodied member(property):
+        public string Surname { 
+            get => this.surname;
+            set => this.surname = value; 
+        }
+
+        //Fully implemented property:
+        private string surnamePrefix;
+        public string SurnamePrefix
+        {
+            get { return surnamePrefix; }
+            set {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException();
+                }    
+                surnamePrefix = value; 
+            }
+        }
+
+
+        //contructor
+        public Person(string surname)
+        {
+            this.Surname = surname;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ ActivityPlanner/Program.cs
@@ -0,0 +1,18 @@
+﻿using ActivityPlanner.Models;
+using System;
+
+namespace ActivityPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jansen");
+            person.Firstname = "";
+            Person p = new Employee("Hans");
+            Employee e = new Person();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
